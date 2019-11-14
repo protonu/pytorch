20f1abda5e297c1a04f5f0d9f90889992df364bf
Rohan Varma <rvarm1@fb.com>
2019-11-14 11:28:49 -0800

    [wip][rpc] poll for timed out futures in process group agent
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/29601
    
    Follow up from https://github.com/pytorch/pytorch/pull/28392. Adds a background thread to `ProcessGroupAgent` that polls for timed out RPCs at a pre-set interval, and marks them as completed with a timeout exception if they have timed out. Also deletes the futures from the corresponding maps `futures_` and `futureTimeouts`. Unit tests are added to ensure that timed out RPCs are appropriately cleaned up.
    
    Also adds a `shutdown` variable to process group agent to control the shutting down of this background thread, which can eventually be extended to use for controlling a clean shutdown of process group agent.
    ghstack-source-id: 93927438
    
    Differential Revision: [D18434215](https://our.internmc.facebook.com/intern/diff/D18434215/)

--- torch/csrc/distributed/rpc/future_message.h
+++ torch/csrc/distributed/rpc/future_message.h
@@ -23,6 +23,16 @@ struct TORCH_API FutureMessage final {
   // If completed() the callback will be invoked in-place.
   void addCallback(const Callback& callback);
 
+  // Get the destination rank of this future.
+  inline int dst() const {
+    return dst_;
+
+  }
+  // Set the destination rank of this future.
+  inline void setDst(int dst) {
+    dst_ = dst;
+  }
+
  private:
   void fireCallbacks();
 
@@ -32,6 +42,7 @@ struct TORCH_API FutureMessage final {
   std::vector<Callback> callbacks_;
   // TODO: make message_ an optional field, and get rid of UNKNOWN message type
   Message message_;
+  int dst_; // destination rank of this future.
 };
 
 } // namespace rpc
